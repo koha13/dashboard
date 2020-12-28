c03a8b4cd743ccd6a19dcbdbbf587916ccf3a70b
koha13 <kohatd13@gmail.com>
2020-12-28 16:50:24 +0700

    remove redundant files

--- test.js
+++
@@ -1,8 +0,0 @@
-let a = [
-	{ name: "koha", id: 1 },
-	{ name: "koha", id: 2 },
-	{ name: "koha", id: 3 },
-	{ name: "koha", id: 4 },
-];
-let r = a.reduce((rs, cur) => [...rs, cur.id], []);
-console.log(r);
